aceafc779d56ceb231ae3f6b718d7f5b70a1b5e8
geekedreaperdev <anandmehta200710@gmail.com>
2026-02-16 23:51:26 +0530

    uc2

---
+++ uc2.java
@@ -0,0 +1,15 @@
+package oopsbannerapp.OOPSBannerApp;
+
+public class uc2 {
+    public static void main(String[] args) {
+
+        System.out.println(" *****    *****    *****    *****  ");
+        System.out.println("*     *  *     *  *     *  *       ");
+        System.out.println("*     *  *     *  *     *  *       ");
+        System.out.println("*     *  *     *  * *** *   *****  ");
+        System.out.println("*     *  *     *  *              * ");
+        System.out.println("*     *  *     *  *             * ");
+        System.out.println(" *****    *****   *         *****  ");
+
+    }
+}
