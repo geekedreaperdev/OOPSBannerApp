6c874deed6cbeb22fe6064508a97fc077ad55be5
geekedreaperdev <anandmehta200710@gmail.com>
2026-02-17 13:17:54 +0530

    uc3

---
+++ uc3.java
@@ -0,0 +1,18 @@
+package oopsbannerapp.OOPSBannerApp;
+
+public class uc3 {
+    public static void main(String[] args) {
+
+        System.out.println(String.join("", "      ***      ***      *****       ***** "));
+        System.out.println(String.join("", "     ** **    ** **    **   **    **      "));
+        System.out.println(String.join("", "    **   **  **   **   **   **   **       "));
+        System.out.println(String.join("", "    **   **  **   **   *****      ***     "));
+        System.out.println(String.join("", "    **   **  **   **   **            **   "));
+        System.out.println(String.join("", "     ** **    ** **    **             **  "));
+        System.out.println(String.join("", "      ***      ***     **        *****    "));
+
+    }
+}
+
+    
+
